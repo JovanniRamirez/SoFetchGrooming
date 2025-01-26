35fb1d73ea0cef39b010448b0874cfd2f990db6e
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2025-01-26 14:33:27 -0800

    Created cart item class

---
+++ SoFetchGrooming/Models/CartItem.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoFetchGrooming.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public int CartItemId { get; set; }
+        
+        [Required]
+        public int CartId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public virtual required Product Product { get; set; }
+
+        [Required]
+        public virtual required ShoppingCart Cart { get; set; }
+    }
+}
