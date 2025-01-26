6f416f396b55eb15b2253859a16d2a5072dc009e
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2025-01-26 14:17:54 -0800

    Create Appointment class

---
+++ SoFetchGrooming/Models/Appointment.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+
+namespace SoFetchGrooming.Models
+{
+    /// <summary>
+    /// Appointment class
+    /// </summary>
+    public class Appointment
+    {
+        [Key]
+        public int appointmentId { get; set; }
+
+        [Required]
+        public int userId { get; set; }
+
+        [Required]
+        public int petId { get; set; }
+
+        [Required]
+        public int serviceTypeId { get; set; }
+
+        [Required]
+        [Display(Name = "Appointment Date")]
+        public DateTime appointmentDate { get; set; }
+
+        [Required]
+        [Display(Name = "Appointment Time")]
+        public DateTime appointmentTime { get; set; }
+    }
+
+}
