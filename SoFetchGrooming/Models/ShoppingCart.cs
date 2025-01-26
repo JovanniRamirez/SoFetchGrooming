46e011fff2e2361f930985d0c7cc9262d3fc0bc9
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2025-01-26 14:34:13 -0800

    Create shopping cart class

---
+++ SoFetchGrooming/Models/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoFetchGrooming.Models
+{
+    public class ShoppingCart
+    {
+        [Key]
+        public int CartId { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public required List<CartItem> Items { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal CartTotal { get; set; }
+    }
+}
