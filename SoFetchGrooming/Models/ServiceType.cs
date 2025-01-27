20340efe4a09bb7355f2393db31346e6c68bb311
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2025-01-27 00:46:17 -0800

    Created the service type class

---
+++ SoFetchGrooming/Models/ServiceType.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoFetchGrooming.Models
+{
+    public class ServiceType
+    {
+        [Key]
+        public int ServiceTypeId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ServiceName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(500)]
+        public string ServiceDescription { get; set; } = string.Empty;
+
+        [Required]
+        public double ServicePrice { get; set; }
+    }
+}
