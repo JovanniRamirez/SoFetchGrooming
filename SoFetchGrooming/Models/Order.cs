09d254e363ee589be9853b8ad00e6442abcd5421
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2025-01-26 22:49:01 -0800

    Created the Order class. Keeps track of date and order status of orders.

---
+++ SoFetchGrooming/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoFetchGrooming.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        
+        public required string OrderStatus { get; set; }
+
+        // Navigation property
+        public virtual required User User { get; set; }
+
+        public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}
