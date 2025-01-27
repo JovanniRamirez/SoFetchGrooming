207fe64636b8892455023aeed46ca1510363390a
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2025-01-27 00:34:00 -0800

    Created Admin Class

---
+++ SoFetchGrooming/Models/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoFetchGrooming.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminId { get; set; }
+
+        [Required]
+        public string AdminName { get; set; }
+
+        [Required]
+        public string AdminEmail { get; set; }
+    }
+}
