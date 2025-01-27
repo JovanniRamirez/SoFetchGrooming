66f4ea16e7df5fd16dcf4e30228cbc04ce860919
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2025-01-26 23:44:23 -0800

    Created a User class

---
+++ SoFetchGrooming/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoFetchGrooming.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string UserName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string UserEmail { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string UserPassword { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string UserPhone { get; set; }
+
+    }
+}
