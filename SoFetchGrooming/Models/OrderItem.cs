c352b07f07acf8e0d7b07a42aaef26794c91cdfb
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2025-01-26 23:06:44 -0800

    Order item class. Keeps track of individual items and quantity for orders. Keeps track of the price of the item at the time of the order

---
+++ SoFetchGrooming/Models/OrderItem.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoFetchGrooming.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public int OrderItemId { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        // Navigation properties
+        public virtual Product Product { get; set; }
+        public virtual Order Order { get; set; }
+    }
+}
