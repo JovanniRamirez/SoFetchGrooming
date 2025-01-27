56620cfd2cb2b03b577e6d2bf98099313f4cdf30
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2025-01-27 00:22:20 -0800

    Created the Pet Class

---
+++ SoFetchGrooming/Models/Pet.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoFetchGrooming.Models
+{
+    public class Pet
+    {
+        [Key]
+        public int PetId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual User? User { get; set; }
+
+        [Required]
+        public required string PetName { get; set; }
+
+        [Required]
+        public required string PetType { get; set; }
+
+        [Required]
+
+        public required string PetBreed { get; set; }
+
+        [Required]
+        public required string PetColor { get; set; }
+
+        [Required]
+        public required string PetWeight { get; set; }
+
+        [Required]
+        public required string PetAge { get; set; }
+
+        [Required]
+        public required string PetGender { get; set; }
+
+        [Required]
+        public required string PetVaccination { get; set; }
+
+        [Required]
+        public required string PetAllergies { get; set; }
+
+        [Required]
+        public required string PetSpecialNeeds { get; set; }
+
+        [Required]
+        public required string PetMedications { get; set; }
+    }
+}
