133cd43137378a7a1ded7d1c3a20a5142b1683fe
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2025-01-26 23:15:37 -0800

    Update method to get total order price. commented out user until I find out if I am going to make a custom user class or use part of .NET user classes

--- SoFetchGrooming/Models/Order.cs
+++ SoFetchGrooming/Models/Order.cs
@@ -18,8 +18,22 @@ namespace SoFetchGrooming.Models
         public required string OrderStatus { get; set; }
 
         // Navigation property
-        public virtual required User User { get; set; }
+        //public virtual required User User { get; set; }
 
         public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+        // Calculate total price of order
+        public decimal OrderTotal
+        {
+            get
+            {
+                decimal total = 0;
+                foreach (OrderItem item in OrderItems)
+                {
+                    total += (item.Quantity * item.Price);
+                }
+                return total;
+            }
+        }
     }
 }
