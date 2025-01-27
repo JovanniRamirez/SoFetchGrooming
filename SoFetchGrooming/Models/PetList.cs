5703975718240238598aff22156a57fcc22d2e3f
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2025-01-27 00:30:07 -0800

    Created pet List and set Key and FK to User class.

---
+++ SoFetchGrooming/Models/PetList.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoFetchGrooming.Models
+{
+    public class PetList
+    {
+        [Key]
+        public int PetListId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual User? User { get; set; }
+
+        public List<Pet> Pets { get; set; } = new List<Pet>();
+    }
+}
