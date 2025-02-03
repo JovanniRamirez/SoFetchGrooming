188afa6b782229e1e2a83adc018c929379e62245
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2025-02-03 01:03:08 -0800

    Migrated the updated User class to database. You might need to update database to get the changes on your local database

---
+++ SoFetchGrooming/Data/Migrations/20250203085658_UpdateUserClass.cs
@@ -0,0 +1,321 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoFetchGrooming.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointment_Users_UserId",
+                table: "Appointment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PetLists_Users_UserId",
+                table: "PetLists");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pets_Users_UserId",
+                table: "Pets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCarts_Users_UserId",
+                table: "ShoppingCarts");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "serviceTypeId",
+                table: "Appointment",
+                newName: "ServiceTypeId");
+
+            migrationBuilder.RenameColumn(
+                name: "petId",
+                table: "Appointment",
+                newName: "PetId");
+
+            migrationBuilder.RenameColumn(
+                name: "appointmentTime",
+                table: "Appointment",
+                newName: "AppointmentTime");
+
+            migrationBuilder.RenameColumn(
+                name: "appointmentDate",
+                table: "Appointment",
+                newName: "AppointmentDate");
+
+            migrationBuilder.RenameColumn(
+                name: "appointmentId",
+                table: "Appointment",
+                newName: "AppointmentId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "ShoppingCarts",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Pets",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "PetLists",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Orders",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Appointment",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointment_AspNetUsers_UserId",
+                table: "Appointment",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AspNetUsers_UserId",
+                table: "Orders",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PetLists_AspNetUsers_UserId",
+                table: "PetLists",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pets_AspNetUsers_UserId",
+                table: "Pets",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCarts_AspNetUsers_UserId",
+                table: "ShoppingCarts",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointment_AspNetUsers_UserId",
+                table: "Appointment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AspNetUsers_UserId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PetLists_AspNetUsers_UserId",
+                table: "PetLists");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pets_AspNetUsers_UserId",
+                table: "Pets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCarts_AspNetUsers_UserId",
+                table: "ShoppingCarts");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "ServiceTypeId",
+                table: "Appointment",
+                newName: "serviceTypeId");
+
+            migrationBuilder.RenameColumn(
+                name: "PetId",
+                table: "Appointment",
+                newName: "petId");
+
+            migrationBuilder.RenameColumn(
+                name: "AppointmentTime",
+                table: "Appointment",
+                newName: "appointmentTime");
+
+            migrationBuilder.RenameColumn(
+                name: "AppointmentDate",
+                table: "Appointment",
+                newName: "appointmentDate");
+
+            migrationBuilder.RenameColumn(
+                name: "AppointmentId",
+                table: "Appointment",
+                newName: "appointmentId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "ShoppingCarts",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Pets",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "PetLists",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Appointment",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserEmail = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    UserPassword = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    UserPhone = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointment_Users_UserId",
+                table: "Appointment",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PetLists_Users_UserId",
+                table: "PetLists",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pets_Users_UserId",
+                table: "Pets",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCarts_Users_UserId",
+                table: "ShoppingCarts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
