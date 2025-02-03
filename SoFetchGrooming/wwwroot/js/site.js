13908698a5438577dc208999c5cdf0f6bd5e2407
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2025-02-02 21:27:40 -0800

    Updated the Home Index View and added a JS function to get a dog fact
    Added a container div to hold 2 rows, with information such as About, Service, a placeholder image, Map, and implemented the Dog Fact API

--- SoFetchGrooming/wwwroot/js/site.js
+++ SoFetchGrooming/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Load A Random Dog fact when the page loads
+document.addEventListener('DOMContentLoaded', function () {
+
+    getRandomDogFact().then(data => {
+        const dogFactsContainer = document.getElementById('dog-facts');
+        dogFactsContainer.textContent = data.data[0].attributes.body;
+    });
+
+});
+
+
+// We are using this API (https://dogapi.dog/docs/api-v2) to get a random dog fact
+async function getRandomDogFact() {
+    
+    // Get the Random Dog Fact and await for a response
+    let response = await fetch('https://dogapi.dog/api/v2/facts');
+    console.log(response);
+
+    // Check if the response is ok
+    if (!response.ok) {
+        alert("Could not retrieve a Fact please try again later");
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    // Parse the response as JSON if the response is ok
+    let data = await response.json();
+    console.log(data);
+    return data;
+};
