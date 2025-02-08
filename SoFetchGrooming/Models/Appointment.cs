a321c59c4aee77977a2c27f75aad33080d68a7a9
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2025-02-08 01:31:42 -0800

    Documented Appointment class and added DB limits on strings

--- SoFetchGrooming/Models/Appointment.cs
+++ SoFetchGrooming/Models/Appointment.cs
@@ -9,27 +9,53 @@ namespace SoFetchGrooming.Models
     /// </summary>
     public class Appointment
     {
+        /// <summary>
+        /// AppointmentId property for the id of the appointment
+        /// </summary>
         [Key]
         public int AppointmentId { get; set; }
 
+        /// <summary>
+        /// UserId property for the id of the user 
+        /// </summary>
         [Required]
         public required string UserId { get; set; }
 
+        /// <summary>
+        /// User property for the user connects the userId to the ApplicationUser
+        /// </summary>
         [ForeignKey("UserId")]
         public virtual required ApplicationUser User { get; set; }
 
-
+        /// <summary>
+        /// PetId property for the id of the pet
+        /// </summary>
         [Required]
+        [Display(Name = "Pet")]
+        [StringLength(50)]
         public int PetId { get; set; }
 
+        /// <summary>
+        /// ServiceTypeId property for the id of the service type
+        /// </summary>
         [Required]
+        [Display(Name = "Service Type")]
+        [StringLength(50)]
         public int ServiceTypeId { get; set; }
 
+        /// <summary>
+        /// AppointmentDate property for the date of the appointment
+        /// </summary>
         [Required]
+        [DataType(DataType.Date)]
         [Display(Name = "Appointment Date")]
         public DateTime AppointmentDate { get; set; }
 
+        /// <summary>
+        /// AppointmentTime property for the time of the appointment
+        /// </summary>
         [Required]
+        [DataType(DataType.Time)]
         [Display(Name = "Appointment Time")]
         public TimeSpan AppointmentTime { get; set; }
     }
