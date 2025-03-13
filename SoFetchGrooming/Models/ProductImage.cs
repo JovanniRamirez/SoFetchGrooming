5657cbe25f4bebe09c54be9ff25770acecd3d5ea
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2025-03-13 13:02:44 -0700

    Did annotation for the ProductImage Class

--- SoFetchGrooming/Models/ProductImage.cs
+++ SoFetchGrooming/Models/ProductImage.cs
@@ -1,12 +1,22 @@
 ﻿namespace SoFetchGrooming.Models
 {
+    /// <summary>
+    /// Product Image Model that stores the image URL for a product
+    /// </summary>
     public class ProductImage
     {
+        // Primary Key for Product Image Table
         public int ProductImageId { get; set; }
+
+        // Image URL for the product
         public required string ImageUrl { get; set; }
 
         //Foreign Key relationship to Product
+
+        // Product ID for the product image
         public int ProductId { get; set; }
+
+        // Navigation property for related product
         public Product Product { get; set; }
     }
 }
