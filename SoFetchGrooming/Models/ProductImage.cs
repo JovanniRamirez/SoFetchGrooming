93ba4634c4adfed6433673812d3aca04d8732051
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2025-03-13 13:00:29 -0700

    Create class to store images as links for the product page

---
+++ SoFetchGrooming/Models/ProductImage.cs
@@ -0,0 +1,12 @@
+﻿namespace SoFetchGrooming.Models
+{
+    public class ProductImage
+    {
+        public int ProductImageId { get; set; }
+        public required string ImageUrl { get; set; }
+
+        //Foreign Key relationship to Product
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
